58be32e077d30e442952c0fd67fbe82d1f6177fe
hjguo48 <hjguo48@gmail.com>
2025-02-01 13:48:51 -0500

    userEntity and JWT

--- src/main/java/com/laioffer/staybooking/model/UserEntity.java
+++ src/main/java/com/laioffer/staybooking/model/UserEntity.java
@@ -2,14 +2,19 @@ package com.laioffer.staybooking.model;
 
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 
 @Entity
 @Table(name = "users")
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -31,14 +36,26 @@ public class UserEntity {
     }
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+
+    @Override
     public String getPassword() {
         return password;
     }
 
 
+    @Override
     public String getUsername() {
         return username;
     }
+
+
+
+
 
 
     public Long getId() {
